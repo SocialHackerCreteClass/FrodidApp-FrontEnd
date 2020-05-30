1667382bf0695178d2c677764913e7bcce686b84
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-05-30 17:58:48 +0300

    created login/logout buttons

--- src/Pages/Auth/Login/index.js
+++ src/Pages/Auth/Login/index.js
@@ -1,11 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 
 Login.propTypes = {}
 
 function Login() {
+  const [userIsLogged, setUserIsLogged] = useState(false)
+  console.log(userIsLogged)
+
+  function login() {
+    setUserIsLogged(true)
+    console.log(userIsLogged)
+  }
+
+  function logout() {
+    setUserIsLogged(false)
+    console.log(userIsLogged)
+  }
+
   return (
     <div>
       <h1>This will be the Login page</h1>
+      <button onClick={login}>Log In</button>
+      <button onClick={logout}>Log Out</button>
+      {userIsLogged && <h1>Welcome to our app!</h1>}
     </div>
   )
 }

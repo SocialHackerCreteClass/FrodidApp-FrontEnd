714776e736a86c2bba06038670948705239ba421
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-06-12 20:39:46 +0300

    Update index.js

--- src/components/LoginStatus/index.js
+++ src/components/LoginStatus/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useI18n } from "providers/I18n"
+import Badge from "./../Badge"
 
 LoginStatus.propTypes = {
   loggedStatus: PropTypes.bool
@@ -19,8 +20,11 @@ function LoginStatus(props) {
       <button onClick={() => updateLocale("en")}>English</button>
       <button onClick={() => updateLocale("el")}>Ελληνικά</button>
       {props.loggedStatus}
+      <div>
+        <Badge />
+      </div>
     </div>
   )
 }
-
+//this is a test commit
 export default LoginStatus

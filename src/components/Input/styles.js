ad4547a3170b30110b19a7288fff0f46d9e4fbe0
Konstantinos Antoniadis <hello@pixelattack.gr>
2020-06-13 18:19:39 +0300

    Transfers input globalstyles to input component

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -19,4 +19,31 @@ export const InputCls = css`
     background-color: var(--global-error-10);
     color: var(--global-error-70);
   }
+  & > input {
+        display: block;
+        width: 100%;
+        padding: 12px;
+        color: var(--global-grey-60);
+        background-color: var(--global-white);
+        border: 1px solid var(--global-grey-10);
+        border-radius: 6px;
+        margin: 0;
+        transition: border-color 0.15s ease-in-out;
+        font-size: 15px;
+        font-size: 0.94rem;
+        line-height: 1.6;
+      }
+
+  & > input[disabled] {
+        cursor: not-allowed;
+      }
+
+& > input:focus {
+      color: var(--primary-90);
+      background-color: var(--global-white);
+      border-color: var(--global-grey-20);
+      outline-width: 3px;
+      outline-style: solid;
+      outline-color: var(--primary-60);
+    }
 `

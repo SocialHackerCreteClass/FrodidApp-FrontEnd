60e444f755eb45c717a47c72aeafe31ea943fbb1
MariaS12 <57732504+MariaS12@users.noreply.github.com>
2020-06-16 19:11:17 +0300

    Professionals-routes (#6)
    * Professionals-routes
    
    * Fix build issue. Add i18n key to the translations file
    
    Co-authored-by: Nikos Kleidis <n.kleidis@logicea.com>

---
+++ src/Pages/Professionals/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { Route, Switch } from "react-router-dom"
+import AllProfessionals from "./AllProfessionals"
+import Edit from "./Edit"
+import Professional from "./Professional"
+
+Professionals.propTypes = {
+  match: PropTypes.object
+}
+
+function Professionals({ match }) {
+  return (
+    <Switch>
+      <Route path={`${match.url}/create-new`} component={Edit} />
+      <Route path={`${match.url}/edit/:id`} component={Edit} />
+      <Route path={`${match.url}/details/:id`} component={Professional} />
+      <Route path={`${match.url}/`} component={AllProfessionals} />
+    </Switch>
+  )
+}
+
+export default Professionals

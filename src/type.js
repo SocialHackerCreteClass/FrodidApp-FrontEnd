e725b4235452dcfc3177d6bf091584f69387f18c
hookah96 <mak96grg@gmail.com>
2020-06-18 14:50:44 +0300

    type of user object

---
+++ src/type.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const UserType = PropTypes.shape({
+    id: PropTypes.number,
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+    birthDate: PropTypes.number.isRequired,
+    createdDate: PropTypes.number.isRequired,
+    afm: PropTypes.number.isRequired,
+    amka: PropTypes.number.isRequired,
+    role: PropTypes.oneOf(["admin", "professional"]).isRequired,
+    profession: PropTypes.string.isRequired,
+    image: PropTypes.string
+})
+   

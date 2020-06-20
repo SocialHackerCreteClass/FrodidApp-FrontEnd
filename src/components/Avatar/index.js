0c19fab7e36e3ace26c47018d8e60f6cda1bf0d3
hookah96 <mak96grg@gmail.com>
2020-06-20 22:08:19 +0300

    first attempt for rendering avatar or letter.

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,22 +1,37 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { cx } from "emotion"
-import { imgWrapper, wrapper } from "./styles"
+import React from "react";
+import PropTypes from "prop-types";
+import { cx } from "emotion";
+import { imgWrapper, wrapper } from "./styles";
+import { UserType } from '../../type';
 
 Avatar.propTypes = {
   className: PropTypes.string,
   size: PropTypes.oneOf(["medium", "large"]),
-  src: PropTypes.string
+  src: UserType.image.PropTypes.string
 }
 
-function Avatar({ size = "medium", src, className }) {
-  return (
-    <div className={cx(wrapper, { large: size === "large" }, className)}>
-      <div className={imgWrapper}>
-        <img src={src} alt="avatar" />
+if ( src !== '' ) {
+  const Avatar = ({ size = "medium", src, className }) => {
+    return (
+      <div className={cx(wrapper, { large: size === "large" }, className)}>
+        <div className={imgWrapper}>
+          <img src={src} alt="avatar" />
+        </div>
       </div>
-    </div>
-  )
+    )}
+} else {
+  const Avatar = () =>{
+  <div class="u-margin-bottom">
+    <a href="#" title="Konstantinos Antoniadis">
+      <span class="c-avatar c-avatar--mobile">
+        <span class="c-avatar__img">
+          <span class="u-uppercase u-bold">KA</span>
+        </span>
+      </span>
+    </a>
+  </div>
+  }
 }
+
 
 export default Avatar

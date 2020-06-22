86d9d0a26fab3a44ec780340a18945bce7f14a83
hookah96 <mak96grg@gmail.com>
2020-06-22 15:14:10 +0300

    Prettier over types.js, Avatar.js, styles.js

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,61 +1,69 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { cx } from "emotion";
-import { imgWrapper, wrapper, marginBottom, avatarMobile, avatarImage, letters } from "./styles";
-import { UserType } from "../../type";
+import React from "react"
+import PropTypes from "prop-types"
+import { cx } from "emotion"
+import {
+  imgWrapper,
+  wrapper,
+  marginBottom,
+  avatarMobile,
+  avatarImage,
+  letters
+} from "./styles"
+import { UserType } from "../../type"
 
 // Renders the Avatar with Image
 const AvatarImage = ({ size = "medium", src, className }) => {
-    return (
-        <div className={cx(wrapper, { large: size === "large" }, className)}>
+  return (
+    <div className={cx(wrapper, { large: size === "large" }, className)}>
       <div className={imgWrapper}>
         <img src={src.image} alt="avatar" />
       </div>
     </div>
-)};
-  
+  )
+}
+
 AvatarImage.propTypes = {
-    className: PropTypes.string,
-    size: PropTypes.oneOf(["medium", "large"]),
-    src: UserType.image.PropTypes.string
-};
+  className: PropTypes.string,
+  size: PropTypes.oneOf(["medium", "large"]),
+  src: UserType.image.PropTypes.string
+}
 
 // Renders avatar with the letters
 const AvatarText = ({ user }) => {
-    const firstNameLetter = user.firstName.slice(0,1);
-    const lastNameLetter = user.lastName.slice(0,1);
-    return (
-        <div className={cx(marginBottom)}>
-        <a href="#" title={`${user.firstName} ${user.lastName}`}>
-            <span className={cx(wrapper, avatarMobile)}>
-                <span className={cx(avatarImage)}>
-                    <span className={cx(letters)}>
-                        {`${firstNameLetter} ${lastNameLetter}`}
-                    </span>
-                </span>
+  const firstNameLetter = user.firstName.slice(0, 1)
+  const lastNameLetter = user.lastName.slice(0, 1)
+  return (
+    <div className={cx(marginBottom)}>
+      <a href="#" title={`${user.firstName} ${user.lastName}`}>
+        <span className={cx(wrapper, avatarMobile)}>
+          <span className={cx(avatarImage)}>
+            <span className={cx(letters)}>
+              {`${firstNameLetter} ${lastNameLetter}`}
             </span>
-        </a>         
+          </span>
+        </span>
+      </a>
     </div>
-)};
-      
-AvatarText.propTypes = {
-    user: UserType
-};
+  )
+}
 
+AvatarText.propTypes = {
+  user: UserType
+}
 
 const Avatar = ({ user }) => {
-    return (
-        <div>
-            {user.image === '' ? (
-                <AvatarText user={user} />
-                ) : (
-                    <AvatarImage src={user.image} />
-                    )}
-        </div>
-    )
+  return (
+    <div>
+      {user.image === "" ? (
+        <AvatarText user={user} />
+      ) : (
+        <AvatarImage src={user.image} />
+      )}
+    </div>
+  )
 }
 
 Avatar.propTypes = {
-    user: UserType
-};
-export default Avatar;
+  user: UserType
+}
+export default Avatar

be3ec182a98a360270a2b5adf690fda25c0ca374
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-07-03 13:06:26 +0300

    used useMemo and added comments to the translations file

--- src/components/Breadcrumbs/index.js
+++ src/components/Breadcrumbs/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React from "react"
+import React, { useMemo } from "react"
 import Breadcrumbs from "react-router-dynamic-breadcrumbs"
 import { itemCls, wrapperCls } from "./styles"
 import { cx } from "emotion"
@@ -8,28 +8,30 @@ import { useI18n } from "providers/I18n"
 function BreadcrumbsComponent() {
   const { t } = useI18n()
 
-  const routesList = {
-    "/login": `${t("int.login")}`,
-    "/register": "Register",
-    "/visits/details": null,
-    "/visits/details/:id": `${t("int.view_single_visit")}`,
-    "/visits/create-new": `${t("int.create_new_visit")}`,
-    "/visits/edit": null,
-    "/visits/edit/:id": `${t("int.edit_a_visit")}`,
-    "/visits": `${t("int.allvisits")}`,
-    "/patients": `${t("int.patients")}`,
-    "/patients/details": null,
-    "/patients/details/:id": `${t("int.patient_details")}`,
-    "/patients/create-new": `${t("int.patients_create_new")}`,
-    "/patients/edit": null,
-    "/patients/edit/:id": `${t("int.patient_edit")}`,
-    "/professionals": `${t("int.professionals")}`,
-    "/professionals/details": null,
-    "/professionals/details/:id": `${t("int.professional_details")}`,
-    "/professionals/create-new": `${t("int.professional_create_new")}`,
-    "/professionals/edit": null,
-    "/professionals/edit/:id": `${t("int.professional_edit")}`
-  }
+  const routesList = useMemo(() => {
+    return {
+      "/login": `${t("int.login")}`,
+      "/register": "Register",
+      "/visits/details": null,
+      "/visits/details/:id": `${t("int.view_single_visit")}`,
+      "/visits/create-new": `${t("int.create_new_visit")}`,
+      "/visits/edit": null,
+      "/visits/edit/:id": `${t("int.edit_a_visit")}`,
+      "/visits": `${t("int.allvisits")}`,
+      "/patients": `${t("int.patients")}`,
+      "/patients/details": null,
+      "/patients/details/:id": `${t("int.patient_details")}`,
+      "/patients/create-new": `${t("int.patients_create_new")}`,
+      "/patients/edit": null,
+      "/patients/edit/:id": `${t("int.patient_edit")}`,
+      "/professionals": `${t("int.professionals")}`,
+      "/professionals/details": null,
+      "/professionals/details/:id": `${t("int.professional_details")}`,
+      "/professionals/create-new": `${t("int.professional_create_new")}`,
+      "/professionals/edit": null,
+      "/professionals/edit/:id": `${t("int.professional_edit")}`
+    }
+  }, [])
 
   return (
     <Breadcrumbs

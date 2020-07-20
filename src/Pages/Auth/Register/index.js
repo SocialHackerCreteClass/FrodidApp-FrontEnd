64571ec44342b467492d251de5c8f41b15e72b00
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-07-20 12:32:36 +0300

    Update index.js

--- src/Pages/Auth/Register/index.js
+++ src/Pages/Auth/Register/index.js
@@ -6,7 +6,7 @@ Register.propTypes = {}
 function Register() {
   return (
     <div>
-      <h1>Users will Register through this page</h1>
+      <h1>Register Page</h1>
       <Link to="/">
         <button>Go back</button>
       </Link>

06910679128108bf2d9e5be61f2ca9a3dcc8cdb0
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-07-28 18:45:57 +0300

    removed console logs

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,5 @@
 export const getUserData = async () => {
-  console.log("function running")
   const response = await fetch(`https://randomuser.me/api/`)
   const data = await response.json()
-  console.log(data.results[0])
   return data.results[0]
 }

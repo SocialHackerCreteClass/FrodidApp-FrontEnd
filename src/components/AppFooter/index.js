8d463d1a7b1c4e7b75b21e2580a21f549ad94596
Konstantinos Antoniadis <hello@pixelattack.gr>
2020-08-04 14:06:32 +0300

    Populates AppFooter with components

--- src/components/AppFooter/index.js
+++ src/components/AppFooter/index.js
@@ -1,49 +1,39 @@
 import React from "react"
 import Grid, { GridCell } from "components/Grid"
-import { wrapper } from './styles'
- 
-function AppFooter(props) {
+import ListInline, { ListInlineItem } from "components/ListInline"
+import { TextIcon } from "components/Flag"
+import MailIcon from "components/Icons/Mail"
+import PhoneIcon from "components/Icons/Phone"
+import { wrapper } from "./styles"
+
+function AppFooter() {
   return (
     <div className={wrapper}>
       <Grid>
-        <GridCell className="u-1/1 u-1/2@l-wide">
-          <div className="u-margin-bottom u-margin-bottom-none@l-wide">
-            <ul className="o-list-inline u-margin-bottom-none">
-              <li className="o-list-inline__item u-margin-right u-margin-bottom-small u-margin-bottom-none@l-wide">
-                <div className="o-flag o-flag--small">
-                  <div className="o-flag__img">
-                    <svg className="c-icon c-icon--small">
-                      <use className="c-icon--white" href="#icon-mail"></use>
-                    </svg>
-                  </div>
-                  <div className="o-flag__body">
-                    <a href="mailto:support@frontidapp.gr" className="u-white">
-                      support@frontidapp.gr
-                    </a>
-                  </div>
-                </div>
-              </li>
-              <li className="o-list-inline__item u-margin-bottom-small u-margin-bottom-none@l-wide">
-                <div className="o-flag o-flag--small">
-                  <div className="o-flag__img">
-                    <svg className="c-icon c-icon--small">
-                      <use className="c-icon--white" href="#icon-phone"></use>
-                    </svg>
-                  </div>
-                  <div className="o-flag__body">
-                    <span className="u-white">0030 2810 223 555</span>
-                  </div>
-                </div>
-              </li>
-            </ul>
+        <GridCell className="u-1_1 u-1_2-l-wide">
+          <div className="u-margin-bottom u-margin-bottom-none-l-wide">
+            <ListInline className="u-margin-bottom-none">
+              <ListInlineItem className="u-margin-right u-margin-bottom-small u-margin-bottom-none-l-wide">
+                <TextIcon icon={<MailIcon size={"small"} color={"white"} />}>
+                  <a href="mailto:support@frontidapp.gr" className="u-white">
+                    support@frontidapp.gr
+                  </a>
+                </TextIcon>
+              </ListInlineItem>
+              <ListInlineItem className="u-margin-bottom-small u-margin-bottom-none-l-wide">
+                <TextIcon icon={<PhoneIcon size={"small"} color={"white"} />}>
+                  <span className="u-white">0030 2810 223 555</span>
+                </TextIcon>
+              </ListInlineItem>
+            </ListInline>
           </div>
         </GridCell>
-        <GridCell className="u-1/1 u-1/2@l-wide">
-          <div className="u-text-align-right@l-wide">
+        <GridCell className="u-1_1 u-1_2-l-wide">
+          <div className="u-text-align-right-l-wide">
             <span>
-              Designed and Developed
+              Designed and Developed by{" "}
               <a href="#" className="u-white">
-                by&nbsp;SHA&nbsp;Heraklion
+                SHA&nbsp;Heraklion
               </a>
             </span>
           </div>

3cf5ed2bbdabd54090685ef815a3d2a8064191a0
Konstantinos Antoniadis <hello@pixelattack.gr>
2020-08-04 13:02:13 +0300

    Populates AppHeaderMobile with components

--- src/components/AppHeaderMobile/index.js
+++ src/components/AppHeaderMobile/index.js
@@ -1,92 +1,71 @@
 import React from "react"
+import LogoIcon from "components/Icons/Logo"
+import Avatar from "components/Avatar"
+import PatientsIcon from "components/Icons/Patients"
+import ProfessionalsIcon from "components/Icons/Professionals"
+import VisitationsIcon from "components/Icons/Visitations"
+import AnalyticsIcon from "components/Icons/Analytics"
+import ListBare from "components/ListBare"
 
-function AppHeaderMobile(props) {
+function AppHeaderMobile() {
   return (
-    <div className="c-app-header c-app__header--mobile u-hide@desktop">
+    <div className="c-app-header c-app__header--mobile u-hide-desktop">
       <div className="u-padding-horizontal-tiny u-padding-vertical-small u-text-align-center">
         <div className="u-margin-bottom">
           <a href="/" title="Frontidapp">
-            <svg className="c-logo c-logo--mobile">
-              <use href="#icon-logo-small"></use>
-            </svg>
+            <LogoIcon />
           </a>
         </div>
         <div className="u-margin-bottom">
           <a href="#" title="Konstantinos Antoniadis">
-            <span className="c-avatar c-avatar--mobile">
-              <span className="c-avatar__img">
-                <span className="u-uppercase u-bold">KA</span>
-              </span>
-            </span>
+            <Avatar
+              user={{ image: null, firstName: "John", lastName: "doe" }}
+            />
           </a>
         </div>
         <nav>
           <h2 className="u-hidden-visually">Navigation</h2>
           <h3 className="u-hidden-visually">Pages</h3>
-          <ul className="o-list-bare u-margin-bottom">
+          <ListBare className="u-margin-bottom">
             <li className="u-margin-bottom-small">
               <a href="patients.html">
-                <svg className="c-icon">
-                  <use className="c-icon--primary" href="#icon-patients"></use>
-                </svg>
+                <PatientsIcon />
               </a>
             </li>
             <li className="u-margin-bottom-small">
               <a href="/professionals">
-                <svg className="c-icon">
-                  <use
-                    className="c-icon--primary"
-                    href="#icon-professionals"></use>
-                </svg>
+                <ProfessionalsIcon />
               </a>
             </li>
             <li className="u-margin-bottom-small">
               <a href="/visitations">
-                <svg className="c-icon">
-                  <use
-                    className="c-icon--primary"
-                    href="#icon-visitations"></use>
-                </svg>
+                <VisitationsIcon />
               </a>
             </li>
             <li className="u-margin-bottom-small">
               <a href="/analytics">
-                <svg className="c-icon">
-                  <use className="c-icon--primary" href="#icon-analytics"></use>
-                </svg>
+                <AnalyticsIcon />
               </a>
             </li>
-          </ul>
+          </ListBare>
           <h3 className="u-hidden-visually">Actions</h3>
-          <ul className="o-list-bare u-margin-bottom">
+          <ListBare className="u-margin-bottom">
             <li className="u-margin-bottom-small">
               <a href="/patients/create-new">
-                <svg className="c-icon">
-                  <use
-                    className="c-icon--secondary"
-                    href="#icon-patients"></use>
-                </svg>
+                <PatientsIcon type={"secondary"} />
               </a>
             </li>
             <li className="u-margin-bottom-small">
               <a href="/professionals/create-new">
-                <svg className="c-icon">
-                  <use
-                    className="c-icon--secondary"
-                    href="#icon-professionals"></use>
-                </svg>
+                <ProfessionalsIcon type={"secondary"} />
               </a>
             </li>
             <li className="u-margin-bottom-small">
               <a href="/visitations/create-new">
-                <svg className="c-icon">
-                  <use
-                    className="c-icon--secondary"
-                    href="#icon-visitations"></use>
-                </svg>
+                <VisitationsIcon type={"secondary"} />
               </a>
             </li>
-          </ul>
+          </ListBare>
         </nav>
       </div>
     </div>

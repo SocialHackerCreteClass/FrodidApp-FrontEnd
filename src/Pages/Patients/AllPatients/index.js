da686d66ab84c9621cffab7ea795acfba3e468b6
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-08-12 16:36:15 +0300

    Update index.js

--- src/Pages/Patients/AllPatients/index.js
+++ src/Pages/Patients/AllPatients/index.js
@@ -1,17 +1,36 @@
 import React from "react"
 import { useI18n } from "providers/I18n"
+import { headerContainer } from "./styles"
+import Button from "../../../../src/components/Button/index.js"
+import { cx } from "emotion"
+import Table from "components/Table/index.js"
+import { data, columns } from "Pages/TestPage/PatientTable"
 
 AllPatients.propTypes = {}
 
 function AllPatients() {
   const { t } = useI18n()
+
   return (
     <div>
-      <h1>{t("int.all-patients-header", "patients")}</h1>
+      <div className={headerContainer}>
+        <div className={cx("u-margin-bottom-large u-margin-bottom-huge@wide")}>
+          <h1 className={"h3 u-margin-bottom-none"}>{`${t(
+            "int.patients"
+          )}`}</h1>
+          <p>{data.length} entries</p>
+        </div>
+        <div>
+          <Button variant="secondary" size="large">
+            <span className={"u-large u-uppercase u-letter-spacing u-bold"}>
+              {`${t("int.patient_new")}`}
+            </span>
+          </Button>
+        </div>
+      </div>
+      {<Table columns={columns} data={data} />}
     </div>
   )
 }
-
-//test comment
 
 export default AllPatients

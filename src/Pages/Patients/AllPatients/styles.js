7577fd10369ec10732d396d4f3042a888cb7355a
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-08-12 16:36:04 +0300

    Create styles.js

---
+++ src/Pages/Patients/AllPatients/styles.js
@@ -0,0 +1,11 @@
+import { css } from "emotion"
+
+export const headerContainer = css`
+  display: flex;
+  flex-flow: row wrap;
+  list-style: none;
+  padding: 0;
+  align-items: start;
+  justify-content: space-between;
+  width: 100%;
+`

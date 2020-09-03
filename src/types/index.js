da7d088db5793e13f7cd878f43028720f657e83b
hookah96 <mak96grg@gmail.com>
2020-09-03 13:33:40 +0300

    Delete .isRequired from types

--- src/types/index.js
+++ src/types/index.js
@@ -2,15 +2,15 @@ import PropTypes from "prop-types"
 
 export const UserType = PropTypes.shape({
   id: PropTypes.number,
-  firstName: PropTypes.string.isRequired,
-  lastName: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+  email: PropTypes.string,
   password: PropTypes.string,
-  birthDate: PropTypes.number.isRequired,
-  createdDate: PropTypes.number.isRequired,
-  afm: PropTypes.number.isRequired,
-  amka: PropTypes.number.isRequired,
-  role: PropTypes.oneOf(["admin", "professional"]).isRequired,
+  birthDate: PropTypes.number,
+  createdDate: PropTypes.number,
+  afm: PropTypes.number,
+  amka: PropTypes.number,
+  role: PropTypes.oneOf(["admin", "professional"]),
   profession: PropTypes.oneOf([
     "Nurse",
     "Social Worker",
@@ -18,37 +18,37 @@ export const UserType = PropTypes.shape({
     "Family Helper",
     "Physiotherapist",
     "Doctor"
-  ]).isRequired,
+  ]),
   image: PropTypes.string
 })
 
 export const PatientType = PropTypes.shape({
   id: PropTypes.number,
-  firstName: PropTypes.string.isRequired,
-  lastName: PropTypes.string.isRequired,
-  birthDate: PropTypes.string.isRequired,
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+  birthDate: PropTypes.string,
   telephone: PropTypes.number,
   mobile: PropTypes.number,
-  amka: PropTypes.number.isRequired,
-  afm: PropTypes.number.isRequired,
+  amka: PropTypes.number,
+  afm: PropTypes.number,
   comments: PropTypes.string,
   gender: PropTypes.oneOf(["male", "female", "other"]),
   address: PropTypes.shape({
-    street: PropTypes.string.isRequired,
+    street: PropTypes.string,
     streetNumber: PropTypes.string,
-    region: PropTypes.string.isRequired,
+    region: PropTypes.string,
     zipCode: PropTypes.string,
-    country: PropTypes.string.isRequired,
-    state: PropTypes.string.isRequired
+    country: PropTypes.string,
+    state: PropTypes.string
   })
 })
 
 export const VisitType = PropTypes.shape({
   id: PropTypes.number,
-  date: PropTypes.number.isRequired,
+  date: PropTypes.number,
   comment: PropTypes.string,
-  startTime: PropTypes.string.isRequired,
-  endTime: PropTypes.string.isRequired,
+  startTime: PropTypes.string,
+  endTime: PropTypes.string,
   user: UserType,
   patient: PatientType
 })

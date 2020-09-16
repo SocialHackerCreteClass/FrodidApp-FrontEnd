e356144ef148a238aeb710e4b15748e4d220b26b
Giorgos Makrakis <mak96grg@gmail.com>
2020-09-16 17:52:55 +0300

    All patient page (#45)
    * Add Professionals page with translation, fix links for actions column in VisitationsTable
    
    * Add All Patients Table, update api/patients func
    
    * minor corrections
    
    * use the same api professionals file
    
    Co-authored-by: Nikos Kleidis <n.kleidis@logicea.com>

--- src/Pages/Patients/AllPatients/index.js
+++ src/Pages/Patients/AllPatients/index.js
@@ -1,19 +1,35 @@
-import React, { useEffect } from "react"
+import React, { useState } from "react"
+import { usePaginatedQuery } from "react-query"
 import { useI18n } from "providers/I18n"
 import { getPatients } from "api/patients"
+
+import PatientsTable from "./PatientTable"
+import AllUsersTitle from "components/AllUsersTitle"
 
 AllPatients.propTypes = {}
 
 function AllPatients() {
-  const { t } = useI18n()
+  const [pageInfo, setPageInfo] = useState({ pageSize: 10, pageIndex: 0 })
+  const { resolvedData = {}, isLoading } = usePaginatedQuery(
+    ["allPatientsData", pageInfo],
+    getPatients
+  )
 
-  useEffect(() => {
-    getPatients({ items_per_page: 5, page_number: 1 })
-  }, [])
+  const { t } = useI18n()
+  if (isLoading) return "Loading..."
 
   return (
     <div>
-      <h1>{t("int.all-patients-header", "patients")}</h1>
+      <AllUsersTitle
+        title={t("int.patients")}
+        total={resolvedData.total}
+        btn={t("int.new_patient")}
+      />
+      <PatientsTable
+        setPageInfo={setPageInfo}
+        resolvedData={resolvedData}
+        pageInfo={pageInfo}
+      />
     </div>
   )
 }

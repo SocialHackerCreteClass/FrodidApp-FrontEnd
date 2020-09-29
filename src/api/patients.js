72b9618b09513779739f9a5ac29631839edd6a92
Nikos Kleidis <nikoskleidis@gmail.com>
2020-09-29 20:06:00 +0300

    Fetch server data for all Patients page and edit page as well (#48)

--- src/api/patients.js
+++ src/api/patients.js
@@ -1,21 +1,67 @@
-import { sleep } from "../utils"
+const URL = "/patients"
 
-// const URL=""
-
-export const getPatients = async (key, { pageSize, pageIndex }) => {
-  //  const data = await fetch(URL)
-  //  const response = await data.json()
-  //  return response
-  await sleep(2000)
-
+export const getPatients = async (key, urlParams) => {
+  const serverData = await window.appFetch(
+    `${URL}?${new URLSearchParams(urlParams).toString()}`
+  )
+  // eslint-disable-next-line no-unused-vars
+  const { total_results, pages_length, total, data } = await serverData.json()
   return {
-    total: mockPatients.total,
-    pageSize,
-    pageIndex,
-    data: mockPatients.data.slice(
-      pageSize * pageIndex,
-      Math.min(mockPatients.data.length, pageSize * pageIndex + pageSize)
-    )
+    total: Number(total),
+    data: data.map(normalizeUser),
+    ...urlParams
+  }
+}
+
+export const getPatient = async (key, id) => {
+  const serverData = await window.appFetch(`${URL}/${id}`)
+  const { data } = await serverData.json()
+  return normalizeUser(data[0])
+}
+
+export const savePatient = async (user) => {
+  const body = JSON.stringify(denormalizeUser(user))
+  let serverData
+  if (user.id) {
+    serverData = await window.appFetch(`${URL}/${user.id}`, {
+      method: "PUT",
+      body
+    })
+  } else {
+    serverData = await window.appFetch(URL, { method: "POST", body })
+  }
+  return serverData
+}
+
+function denormalizeUser(user) {
+  const {
+    // eslint-disable-next-line no-unused-vars
+    birthDate = new Date().getTime(),
+    firstName,
+    lastName,
+    // eslint-disable-next-line no-unused-vars
+    address,
+    // eslint-disable-next-line no-unused-vars
+    gender,
+    ...restProps
+  } = user
+  return {
+    ...restProps,
+    birth_date: "2000-01-31",
+    first_name: firstName,
+    last_name: lastName,
+    address_id: 1,
+    gender_id: 2
+  }
+}
+
+function normalizeUser(serverUser) {
+  const { birth_date, first_name, last_name, ...restProps } = serverUser
+  return {
+    ...restProps,
+    birthDate: birth_date * 1000,
+    firstName: first_name,
+    lastName: last_name
   }
 }
 
